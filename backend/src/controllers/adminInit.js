4ccedca61160f784651b414f6da815eb1e5b8007
wafa <63404321+Oueslati-Waafa@users.noreply.github.com>
2023-09-04 09:40:44 +0100

    test

--- backend/src/controllers/adminInit.js
+++ backend/src/controllers/adminInit.js
@@ -6,6 +6,7 @@ import Admin from "../models/admin.js";
 // this function is used to create the admin account
 export const initializeAdmin = async (username, password,email) => {
   try {
+    
     const existingAdmin = await Admin.findOne({ username });
     if (existingAdmin) {
       console.log("Admin already exists.");
